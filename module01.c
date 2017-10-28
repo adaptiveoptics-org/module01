9c7fdc843a3294baf4802f4187c6a111d625880a
oguyon <oliv.guyon@gmail.com>
2017-10-28 12:05:03 -1000

    sync

---
+++ module01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int module01_func0()
+{
+printf("this is func0, module01\n");
+
+return 0;
+}
---
+++ module01.h
@@ -0,0 +1,7 @@
+#ifndef _MODULE01
+#define MODULE01
+
+int module01_func0();
+
+#endif
+
